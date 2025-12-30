04360ab4818fe5a69412b86f2c5f7a3ab4aa69ef
Kássio <103225180+KassioB@users.noreply.github.com>
2025-12-30 13:10:42 -0300

    revert: restaura estado anterior removendo layout e globals.css que causaram tela branca

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from 'react'
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from './routes'
-import { Layout } from './components/Layout'
 
 function App() {
     return (
         <BrowserRouter basename="/Mente-Aberta">
-            <Layout>
-                <AppRoutes />
-            </Layout>
+            <AppRoutes />
         </BrowserRouter>
     )
 }
